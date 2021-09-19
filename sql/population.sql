7ceceb40e9e8df79c4f9b3ce8cbb7ac08067ba80
Dany <zdanymc@gmail.com>
2021-09-19 16:33:00 +0200

    adding roles

--- sql/population.sql
+++ sql/population.sql
@@ -1,15 +1,20 @@
 START TRANSACTION;
 
-INSERT INTO utenti(Nome, Cognome) VALUES("Daniele", "Nicosia");
-INSERT INTO utenti(Nome, Cognome) VALUES("Vincenzo", "D'Angelo");
+DELETE FROM utenti;
+DELETE FROM staff;
+DELETE FROM squadre;
+DELETE FROM lavoratori;
+DELETE FROM ruoli;
 
-INSERT INTO staff(SID, Nickname, Password, PL) VALUES(1, "alwe", SHA2("alwe", 256), 1);
-INSERT INTO staff(SID, Nickname, Password, PL) VALUES(2, "vinc", SHA2("dang", 256), 2);
+INSERT INTO ruoli(servizio, livello) VALUES("Tutto", 1), ("Grest", 1);
 
-INSERT INTO squadre(Nome, Anno) VALUES("Boh", YEAR(CURRENT_DATE()));
-INSERT INTO squadre(Nome, Anno) VALUES("Lol", 2020);
+INSERT INTO utenti(Nome, Cognome) VALUES("Daniele", "Nicosia"), ("Vincenzo", "D'Angelo");
 
-INSERT INTO lavoratori(LID, Squadra, Anno, Occupazione) VALUES(2, 1, YEAR(CURRENT_DATE()), 1);
+INSERT INTO staff(SID, Nickname, Password, ruolo) VALUES(1, "alwe", SHA2("alwe", 256), 1), (2, "vinc", SHA2("dang", 256), 2);
+
+INSERT INTO squadre(Nome, Anno) VALUES("Boh", YEAR(CURRENT_DATE())), ("Lol", 2020);
+
+INSERT INTO lavoratori(LID, Squadra, Anno, ruolo) VALUES(2, 1, YEAR(CURRENT_DATE()), 2);
 
 -- SELECT * FROM giorni WHERE DATE(Data) BETWEEN "YEAR-01-01" AND "YEAR-12-31" -- per prendere i valori di un anno del grest
 
