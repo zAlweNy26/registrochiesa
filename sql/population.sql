292059a0fa11692b300c9a94b2e8de47e5efd1ad
Dany <zdanymc@gmail.com>
2021-09-19 18:14:45 +0200

    added random function

--- sql/population.sql
+++ sql/population.sql
@@ -5,6 +5,7 @@ DELETE FROM staff;
 DELETE FROM squadre;
 DELETE FROM lavoratori;
 DELETE FROM ruoli;
+DELETE FROM partecipanti;
 
 INSERT INTO ruoli(servizio, livello) VALUES("Tutto", 1), ("Grest", 1);
 
@@ -15,6 +16,8 @@ INSERT INTO staff(ID, Nickname, Password, ruolo) VALUES(1, "alwe", SHA2("alwe", 
 INSERT INTO squadre(Nome, Anno) VALUES("Boh", YEAR(CURRENT_DATE())), ("Lol", 2020);
 
 INSERT INTO lavoratori(ID, Squadra, Anno, ruolo) VALUES(2, 1, YEAR(CURRENT_DATE()), 2);
+
+INSERT INTO partecipanti(ID, Squadra, Anno, UUID) VALUES(2, 1, YEAR(CURRENT_DATE()), RINT(1, 65535));
 
 -- SELECT * FROM giorni WHERE DATE(Data) BETWEEN "YEAR-01-01" AND "YEAR-12-31" -- per prendere i valori di un anno del grest
 
