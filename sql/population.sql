0e28b5d9bb0f4051d0b6cb3821dac6e1b362201d
Dany <zdanymc@gmail.com>
2021-09-30 16:37:54 +0200

    aggiunto capo alla squadra

--- sql/population.sql
+++ sql/population.sql
@@ -25,19 +25,19 @@ INSERT INTO staff(ID, nickname, password, ruolo) VALUES(1, "alwe", SHA2("alwe", 
 
 INSERT INTO anni(servizio, anno) VALUES("Doposcuola", YEAR(CURRENT_DATE())), ("Grest", 2022), ("Danza", 2020);
 
-INSERT INTO squadre(nome, anno) VALUES("Boh", 1), ("Lol", 2);
+INSERT INTO squadre(nome, anno, capo) VALUES("Boh", 1, 2), ("Lol", 2, 2);
 
 INSERT INTO lavoratori(ID, squadra, anno, ruolo) VALUES(2, 1, 2, 2);
 
 INSERT INTO partecipanti(ID, squadra, anno, accompagnatore) VALUES(1, NULL, 1, NULL), (1, 2, 2, NULL);
 
-INSERT INTO giorni(gdata, ID, temperatura, assente, motivo, comportamento) VALUES("2021-01-01", 1, 36.8, 0, NULL, 1);
+INSERT INTO giorni(gdata, ID, temperatura, assente, motivo, comportamento) VALUES("2021-01-01", 1, 36.8, 0, NULL, 1), VALUES("2022-01-01", 1, 38.2, 1, "Febbre", NULL);
 
 INSERT INTO programma(pdata, descrizione, servizio) VALUES("2021-01-01", "Cazzeggio", "Doposcuola"), ("2022-01-01", "Boh", "Grest");
 
 -- SELECT DI PROVA :
 
--- SELECT * FROM programma WHERE DATE(data) BETWEEN "YEAR-01-01" AND "YEAR-12-31" -- per prendere i valori di un anno del grest
+-- SELECT * FROM programma INNER JOIN giorni ON pdata BETWEEN "YEAR-01-01" AND "YEAR-12-31" AND pdata = gdata AND ID = ?; -- per prendere i valori di un anno del grest
 
 -- SELECT S.nome FROM squadre S, lavoratori L WHERE L.anno = 2 AND S.ID = L.squadra
 
