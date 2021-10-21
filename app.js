3325697f3e10a4829a27acb6fd008deccab0db90
Dany <zdanymc@gmail.com>
2021-10-21 11:56:38 +0200

    started making error page

--- app.js
+++ app.js
@@ -71,6 +71,14 @@ app.use('/', infoRouter)
 app.use('/login', loginRouter)
 app.use('/logbook', logbookRouter)
 
+app.get('*', (req, res) => {
+  res.render('error', {
+    theme: req.session.theme,
+    message: "Minchia fai ?!",
+    error: {status: 404, stack: 0}
+  })
+});
+
 app.use((req, res, next) => next(createError(404)))
 
 app.use((err, req, res, next) => {
