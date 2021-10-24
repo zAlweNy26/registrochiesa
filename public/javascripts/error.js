46d9012e66c1cd87dd770f24c8df4739b52a59e9
Dany <zdanymc@gmail.com>
2021-10-24 11:21:16 +0200

    added error page

--- public/javascripts/error.js
+++ public/javascripts/error.js
@@ -1,14 +1,1 @@
-/*$(window).on('resize', () => {
-    const ratio = 45 / ($(window).innerWidth() / $(window).innerHeight())
-    $("h1").css({
-        transform: "translate(-50%, -50%) rotate(-" + ratio + "deg)"
-    })
-});
-
-$(window).on('load', () => {
-    const ratio = 45 / ($(window).innerWidth() / $(window).innerHeight())
-    $("h1").css({
-        transform: "translate(-50%, -50%) rotate(-" + ratio + "deg)"
-    })
-});*/
 
