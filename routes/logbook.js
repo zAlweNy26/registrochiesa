e27dff2b1b1436b597778db88927ed18b540f0e6
Dany <zdanymc@gmail.com>
2021-10-30 16:36:24 +0200

    fullname

--- routes/logbook.js
+++ routes/logbook.js
@@ -4,18 +4,16 @@ const { doQuery } = require('../functions')
 
 router.get('/', async (req, res, next) => {
   if (req.session.logged) {
-    let activities = []
+    let activities = [], fullName = ""
     await doQuery('SELECT * FROM anni INNER JOIN ruoli ON anni.servizio = ruoli.servizio AND ruoli.ID = ?', [req.session.role], true).then(rs => {
       rs.forEach(act => activities.push({ID: act.ID, service: act.servizio})) // aggiungere suddivisione per periodi
     }).catch(err => res.send(err))
     await doQuery('SELECT nome, cognome FROM utenti WHERE UID = ?', [req.session.UID]).then(rs => {
-      req.session.name = rs.nome
-      req.session.surname = rs.cognome
+      fullName = rs.nome + " " + rs.cognome
     }).catch(err => res.send(err))
     if (req.session.theme == null) req.session.theme = 'lightTheme'
     res.render('logbook', {
-      name: req.session.name,
-      surname: req.session.surname,
+      fullName: fullName,
       services: activities,
       tables: [1, 2, 3, 4, 5, 6],
       theme: req.session.theme
@@ -24,7 +22,8 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/logout', (req, res, next) => {
-  req.session.destroy()
+  //req.session.destroy()
+  
   res.redirect('/login')
 })
 
