ab946909ddad7e90a4e475503117672c38895f1c
Dany <zdanymc@gmail.com>
2022-03-27 21:54:39 +0200

    Update population.sql

--- sql/population.sql
+++ sql/population.sql
@@ -1,3 +1,5 @@
+START TRANSACTION;
+
 DELETE FROM partecipanti;
 DELETE FROM lavoratori;
 DELETE FROM programma;
@@ -9,32 +11,39 @@ DELETE FROM ruoli;
 DELETE FROM staff;
 DELETE FROM anni;
 
+INSERT INTO servizi(nome) VALUES("Doposcuola"), ("Grest"), ("Danza"), ("Inglese"), ("Jujitsu"), ("Calcio");
+
+INSERT INTO ruoli(ID, servizio, livello) VALUES(1, "Doposcuola", 1), (1, "Grest", 1), (2, "Grest", 1); -- DA COMPLETARE
+
 -- INSERT DI PROVA :
 
-INSERT INTO servizi(nome, descrizione) VALUES
-("Doposcuola", "lol"), ("Grest", "idk"), ("Danza", "ahah"), ("Inglese", "meme"), 
-("Jujitsu", "shit"), ("Calcio", "nigga"), ("Gita", "what");
+INSERT INTO utenti(nome, cognome) VALUES("Daniele", "Nicosia"), ("Vincenzo", "D'Angelo");
 
-INSERT INTO ruoli(ID, servizio, livello) VALUES (1, "Doposcuola", 1), (1, "Grest", 1), (2, "Grest", 1); -- DA COMPLETARE
+INSERT INTO staff(ID, nickname, password, ruolo) VALUES(1, "alwe", SHA2("alwe", 256), 1), (2, "vinc", SHA2("dang", 256), 2);
 
-INSERT INTO utenti(nome, cognome) VALUES ("Daniele", "Nicosia"), ("Vincenzo", "D'Angelo");
+INSERT INTO anni(servizio, anno) VALUES("Doposcuola", YEAR(CURRENT_DATE())), ("Grest", 2022), ("Danza", 2020);
 
-INSERT INTO anni(servizio, prezzo, dataInizio, dataFine) VALUES
-("Doposcuola", 15, "2021-10-01", "2022-05-31"), 
-("Grest", 10, "2022-06-01", "2022-08-31"), 
-("Danza", 20, "2020-01-01", "2020-05-31");
+INSERT INTO squadre(nome, anno, capo) VALUES("Boh", 1, 2), ("Lol", 2, 2);
 
-INSERT INTO programma(pdata, descrizione, servizio) VALUES ("2021-10-01", "Cazzeggio", "Doposcuola"), ("2022-06-01", "Boh", "Grest");
+INSERT INTO lavoratori(ID, squadra, anno, ruolo) VALUES(2, 1, 2, 2);
 
-INSERT INTO staff(ID, nickname, password, ruolo) VALUES (1, "alwe", SHA2("alwe", 256), 1), (2, "vinc", SHA2("dang", 256), 2);
+INSERT INTO partecipanti(ID, squadra, anno, accompagnatore) VALUES(1, NULL, 1, NULL), (1, 2, 2, NULL);
 
--- DA AGGIUNGERE DOPO IN BASE AL UID
+INSERT INTO giorni(gdata, ID, temperatura, assente, motivo, comportamento) VALUES("2021-01-01", 1, 36.8, 0, NULL, 1), VALUES("2022-01-01", 1, 38.2, 1, "Febbre", NULL);
 
-INSERT INTO squadre(nome, anno, capo) VALUES ("Boh", 1, 2), ("Lol", 2, 2);
+INSERT INTO programma(pdata, descrizione, servizio) VALUES("2021-01-01", "Cazzeggio", "Doposcuola"), ("2022-01-01", "Boh", "Grest");
 
-INSERT INTO lavoratori(UID, squadra, anno) VALUES (2, 1, 2);
+-- SELECT DI PROVA :
 
-INSERT INTO partecipanti(UID, squadra, anno, accompagnatore) VALUES (1, NULL, 1, NULL), (1, 2, 2, NULL);
+-- Prende servizio e anno dal value del dropdown menu (ovvero gli ID dei vari anni a cui ha partecipato l'utente)
+-- SELECT * FROM anni WHERE ID = ?;
 
-INSERT INTO giorni(gdata, UID, temperatura, assente, motivo, comportamento) VALUES
-("2021-10-01", 1, 36.8, 0, NULL, 1), ("2022-06-01", 1, 38.2, 1, "Febbre", NULL);
+-- Controllare se servizio = Grest
+-- Se sì : prende nome squadra e capo squadra con la query
+-- SELECT nome, capo FROM squadre WHERE ID = ?;
+-- Se no : non fare nulla
+
+-- Prende valori da giorni e programma nell'arco di due date per uno specifico utente
+-- SELECT * FROM programma INNER JOIN giorni ON pdata BETWEEN "YEAR-01-01" AND "YEAR-12-31" AND pdata = gdata AND ID = ?;
+
+COMMIT;
